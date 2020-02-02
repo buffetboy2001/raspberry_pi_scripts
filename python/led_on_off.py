4b84b40481c02c99aeeb0db8acf1d7393cc751d8
bowman.d.stuart@gmail.com <bowman.d.stuart@gmail.com>
2020-02-01 19:38:26 -0500

    working led script

--- python/led_on_off.py
+++ python/led_on_off.py
@@ -3,10 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
+GPIO.setup(14,GPIO.OUT)
+print("LED on")
+GPIO.output(14,GPIO.HIGH)
 time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+print("LED off")
+GPIO.output(14,GPIO.LOW)
 

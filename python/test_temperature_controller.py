392ecfeddeec4363be67cfb73b6d98e720cb4ae5
bowman.d.stuart@gmail.com <bowman.d.stuart@gmail.com>
2020-02-01 21:59:45 -0500

    reworked script a bit

--- python/test_temperature_controller.py
+++ python/test_temperature_controller.py
@@ -7,9 +7,29 @@ import temperature_controller as tc
 import time
 
 STARTING_ROOM_TEMPERATURE_C = 19.1  # deg
-CURRENT_ROOM_TEMPERATURE_C = None
 
-def get_temperature_c():
+def main(starting_temp_c:float):
+    '''
+    Run the test
+    '''
+    current_room_temp = starting_temp_c
+
+    while True:
+        current_room_temp_c = get_temperature_c(current_room_temp_c)
+        print('temp: {}'.format(current_room_temp_c))
+
+        if current_room_temp_c > 24.0 and tc.POWER_PIN_STATE:
+                # power is on and it should be turned off
+                toggle_power()
+        elif current_room_temp_c < 22.0 and not tc.POWER_PIN_STATE:
+                # power is off and it should be turned on
+                toggle_power()
+
+        time.sleep(tc.SLEEP_DURATION_SECONDS)
+
+    return
+
+def get_temperature_c(current_room_temp_c:float):
     '''
     Use a simple model to return a temperature.
     '''
@@ -17,33 +37,12 @@ def get_temperature_c():
 
     if tc.POWER_PIN_STATE:
         # the power is on, so increase the room temp
-        CURRENT_ROOM_TEMPERATURE_C = CURRENT_ROOM_TEMPERATURE_C + modeled_temperature_rate*tc.SLEEP_DURATION_SECONDS
+        current_room_temp_c += modeled_temperature_rate*tc.SLEEP_DURATION_SECONDS
     else:
         # the power is off, so decrease the room temp
-        CURRENT_ROOM_TEMPERATURE_C = CURRENT_ROOM_TEMPERATURE_C - modeled_temperature_rate*tc.SLEEP_DURATION_SECONDS
+        current_room_temp_c -= modeled_temperature_rate*tc.SLEEP_DURATION_SECONDS
 
-    return
-
-def main():
-    '''
-    Run the test
-    '''
-    CURRENT_ROOM_TEMPERATURE_C = STARTING_ROOM_TEMPERATURE_C
-
-    while True:
-        get_temperature_c()
-        print('temp: {}'.format(CURRENT_ROOM_TEMPERATURE_C))
-
-		if CURRENT_ROOM_TEMPERATURE_C > 24.0 and tc.POWER_PIN_STATE:
-			# power is on and it should be turned off
-			toggle_power()
-		elif CURRENT_ROOM_TEMPERATURE_C < 22.0 and not tc.POWER_PIN_STATE:
-			# power is off and it should be turned on
-			toggle_power()
-
-		time.sleep(tc.SLEEP_DURATION_SECONDS)
-
-    return
+    return current_room_temp_c
 
 if __name__ == "__main__":
-	main()
+    main(STARTING_ROOM_TEMPERATURE_C)
